8cd7ed1064a9eefc2f7e7ade19a99261abd1b397
Lej <120243069+OnlyLej@users.noreply.github.com>
2025-12-06 12:19:19 +0800

    Update AboutCard.js

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -32,7 +32,7 @@ function AboutCard() {
             </li>
           </ul>
 
-          <p style={{ color: "rgb(155 126 172)" }}>
+          <p style={{ color: "rgb(101 127 163)" }}>
             "Keep going. Small steps still move you forward!"{" "}
           </p>
           <footer className="blockquote-footer">Lejel</footer>
