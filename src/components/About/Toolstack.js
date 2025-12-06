d69160855056e5bdbb7f71b9428017d2f85accfd
Lej <120243069+OnlyLej@users.noreply.github.com>
2025-12-06 10:51:32 +0800

    Update Toolstack.js

--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -8,22 +8,13 @@ import intelliJ from "../../Assets/TechIcons/intellij-idea.svg";
 function Toolstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      <Col xs={4} md={2} className="tech-icons">
-        <img src={macOs} alt="macOs" className="tech-icon-images" />
-        <div className="tech-icons-text">Mac Os</div>
-      </Col>
       <Col xs={4} md={2} className="tech-icons ">
         <img src={chrome} alt="Chrome" className="tech-icon-images" />
         <div className="tech-icons-text">Google Chrome</div>
       </Col>
       <Col xs={4} md={2} className="tech-icons ">
         <img src={vsCode} alt="vsCode" className="tech-icon-images" />
-        <div className="tech-icons-text">Vs Code</div>
-      </Col>
-
-      <Col xs={4} md={2} className="tech-icons ">
-        <img src={intelliJ} alt="go" className="tech-icon-images" />
-        <div className="tech-icons-text">IntelliJ</div>
+        <div className="tech-icons-text">VS Code</div>
       </Col>
     </Row>
   );
