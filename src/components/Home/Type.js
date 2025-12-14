6a3db33791db6b838e88d916f97e7450ed93a758
Lej <120243069+OnlyLej@users.noreply.github.com>
2025-12-14 09:54:52 +0800

    Update Type.js

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -6,8 +6,16 @@ function Type() {
     <Typewriter
       options={{
         strings: [
-          "Programmer",
-          "Electronics Enthusiast",
+         "Programmer",
+         "Electronics Enthusiast",
+         "Embedded Systems Tinkerer",
+         "Microcontroller Developer",
+         "Hardware & Firmware Builder",
+         "IoT Experimenter",
+         "Problem Solver",
+         "Tech Explorer",
+         "DIY Engineer",
+         "System Debugger"
         ],
         autoStart: true,
         loop: true,
