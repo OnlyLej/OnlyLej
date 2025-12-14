87c4145262cdead70502517fb9f662fcd96b8da7
Lej <120243069+OnlyLej@users.noreply.github.com>
2025-12-14 10:21:49 +0800

    Create SecurityCheck.js

---
+++ src/components/SecurityCheck.js
@@ -0,0 +1,22 @@
+import Turnstile from "@marsidev/react-turnstile";
+import { useState } from "react";
+
+export default function SecurityCheck({ onVerified }) {
+  const [verified, setVerified] = useState(false);
+
+  const handleVerify = (token) => {
+    setVerified(true);
+    onVerified();
+  };
+
+  if (verified) return null;
+
+  return (
+    <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
+      <Turnstile
+        siteKey={process.env.REACT_APP_TURNSTILE_SITE_KEY} // <-- use env variable
+        onVerify={handleVerify}
+      />
+    </div>
+  );
+}
