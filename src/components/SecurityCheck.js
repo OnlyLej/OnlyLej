48fba7e757842ab8be9bd64d0a5f5253dc8c169d
Lej <120243069+OnlyLej@users.noreply.github.com>
2025-12-18 06:38:56 +0800

    Update SecurityCheck.js

--- src/components/SecurityCheck.js
+++ src/components/SecurityCheck.js
@@ -1,17 +1,20 @@
 import { Turnstile } from "@marsidev/react-turnstile";
-import { useState } from "react";
 
 export default function SecurityCheck({ onVerified }) {
-  const [verified, setVerified] = useState(false);
-
-  const handleSuccess = (token) => {
-    setVerified(true);
+  const handleSuccess = () => {
     onVerified();
   };
 
-  if (verified) return null;
-
   return (
-    <Turnstile sitekey="0x4AAAAAACGjTZRbda3DxCbm" className='fixed bottom-4 right-4' options={{ theme: 'auto', size: 'normal', language: 'auto', }} scriptOptions={{ appendTo: 'body' }} onSuccess={() => handleSuccess()}/>
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Turnstile siteKey="0x4AAAAAACGjTZRbda3DxCbm" options={{ theme: 'auto', size: 'normal', language: 'auto', }} scriptOptions={{ appendTo: 'body' }} onSuccess={handleSuccess} />
+    </div>
   );
 }
