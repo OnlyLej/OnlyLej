224fbc9bdb855340771cb220a736590b7f5c6941
Lej <120243069+OnlyLej@users.noreply.github.com>
2025-12-18 06:14:59 +0800

    Update SecurityCheck.js

--- src/components/SecurityCheck.js
+++ src/components/SecurityCheck.js
@@ -4,7 +4,7 @@ import { useState } from "react";
 export default function SecurityCheck({ onVerified }) {
   const [verified, setVerified] = useState(false);
 
-  const handleVerify = (token) => {
+  const handleSuccess = (token) => {
     setVerified(true);
     onVerified();
   };
@@ -12,10 +12,17 @@ export default function SecurityCheck({ onVerified }) {
   if (verified) return null;
 
   return (
-    <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
       <Turnstile
-       siteKey={process.env.NEXT_PUBLIC_TURNSTILE_SITE_KEY}
-       onVerify={handleVerify}
+        siteKey={process.env.NEXT_PUBLIC_TURNSTILE_SITE_KEY}
+        onSuccess={handleSuccess}
       />
     </div>
   );
